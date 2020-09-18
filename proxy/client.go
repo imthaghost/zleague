7a237b35dcbc099d75403e05283e40619b820735
Ghost <imthaghost@protonmail.com>
2020-09-18 03:03:40 -0700

    [update] added proxy package for request pool over smartproxy && added proxy configuration functions

---
+++ proxy/client.go
@@ -0,0 +1,83 @@
+package proxy
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+	"zleague/api/config"
+)
+
+/*
+
+If several goroutines are sending requests,
+new connections will be created the pool has all
+connections busy and will create new ones.
+We limit the maximum number of connections per host.
+
+*/
+var (
+	MaxIdleConnections = 10         // Max Idle Connections
+	once               sync.Once    // sync
+	netClient          *http.Client // client
+)
+
+/*
+
+Transport has its own connection pool,
+by default each connection in that pool is
+reused if body is fully read and closed
+*/
+
+// newProxy will returns a proxy function for use
+// in a transport that always returns the same URL
+func newProxy() func(*http.Request) (*url.URL, error) {
+	// credentials from config file
+	c := config.GetProxyConfig()
+	// base url
+	base := "%s://%s:%s@%s"
+	// fill credentials into url
+	proxyURL := fmt.Sprintf(base, c.Schema, c.Username, c.Password, c.Address)
+	// parse proxy url
+	link, err := url.Parse(proxyURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// proxy function
+	proxy := http.ProxyURL(link)
+	// proxy
+	return proxy
+}
+
+/*
+
+Transport is the struct that holds connections for re-use;
+Creating new Transport for each request, will create new connections each time.
+In this case the solution is to share the one Transport instance between clients.
+By default, Transport caches connections for future re-use.
+Reference: https://godoc.org/net/http#Transport
+
+*/
+
+// newNetClient creates a  client
+func newNetClient() *http.Client {
+	once.Do(func() {
+		// transport configuratin
+		var netTransport = &http.Transport{
+			Proxy:        newProxy(),         // default - rotating IP addresses
+			MaxIdleConns: MaxIdleConnections, // max idle connections
+			Dial: (&net.Dialer{ // Dialer
+				Timeout: 20 * time.Second, // max dialer timeout
+			}).Dial,
+			TLSHandshakeTimeout: 20 * time.Second, // transport layer security max timeout
+		}
+		netClient = &http.Client{
+			Timeout:   time.Second * 20, // roundtripper timeout
+			Transport: netTransport,     // how our HTTP requests are made
+		}
+	})
+	// client
+	return netClient
+}
