8805e9007d3a014defef0ab05c50e5a5a9d7c240
omarsagoo <om.sagoo@gmail.com>
2020-09-19 15:45:44 -0700

    stash theses changes

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import "zleague/api/server"
+import (
+	"fmt"
+	"net/http"
+)
+
+func worker(channel string chan){
+for s := range channel {
+	resp, err := http.Get(s)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(resp.StatusCode)
+		err := resp.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+}
+}
 
 func main() {
 	// start the server
-	server := server.NewServer(nil)
-	server.Start(":8080")
+	// server := server.NewServer(nil)
+	// server.Start(":8080")
+	urlchan := make(string chan)
+	url := "https://api.tracker.gg/api/v2/warzone/standard/profile/atvi/onesicksikh%231221896"
+
+	for i := 0; i < 1000; i++ {
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(resp.StatusCode)
+	}
 }
