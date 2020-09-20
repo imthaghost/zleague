7ac2d0dac648367cd786f669b09b32250a2184c6
Ghost <imthaghost@protonmail.com>
2020-09-20 02:20:59 -0700

    [update] check if teams are valid routing logic

--- handlers/teams.go
+++ handlers/teams.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/csv"
 	"html"
 	"log"
 	"net/http"
+	"strings"
+	"zleague/api/cod"
 	"zleague/api/models"
 	"zleague/api/tournament"
 
@@ -36,4 +39,52 @@ func (h *Handler) GetTeam(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, team)
+}
+
+type InvalidTeams struct {
+	Invalid []string
+}
+
+// CheckTeams returns a map of team and player who are invalid
+// TODO move logic out of route
+func (h *Handler) CheckTeams(c echo.Context) error {
+	teamMap := map[string]InvalidTeams{}
+	// csv file
+	csvFormFile, err := c.FormFile("csv")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid CSV Uploaded")
+	}
+
+	csvData, err := csvFormFile.Open()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Error Opening CSV")
+	}
+
+	lines, err := csv.NewReader(csvData).ReadAll()
+	if err != nil {
+		log.Println("Cannot create new Reader ")
+	}
+
+	for _, line := range lines {
+		team := line[1]
+		player := strings.Replace(line[2], "#", "%23", -1)
+		if !cod.IsValid(player) {
+			// teamname is present in map
+			if _, ok := teamMap[team]; ok {
+				t := teamMap[team]                    // reference map
+				t.Invalid = append(t.Invalid, player) // append player to appropriate team
+				teamMap[team] = t
+			} else { // not present in map
+				p := InvalidTeams{}                   // create new struct reference
+				p.Invalid = append(p.Invalid, player) // append teammate
+				teamMap[team] = p                     // create key value pair
+			}
+		}
+	}
+	// empty map
+	if len(teamMap) == 0 {
+		return c.JSON(http.StatusOK, nil)
+	}
+
+	return c.JSON(http.StatusOK, teamMap)
 }
